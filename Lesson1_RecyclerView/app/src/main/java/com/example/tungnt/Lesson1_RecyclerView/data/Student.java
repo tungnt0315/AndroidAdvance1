9cd06456e9cd0bfa8250f02ceb682ae9fca82625
tungnt0315 <tungnt0315@gmail.com>
2017-05-15 10:00:02 +0700

    RecyclerView

---
+++ Lesson1_RecyclerView/app/src/main/java/com/example/tungnt/Lesson1_RecyclerView/data/Student.java
@@ -0,0 +1,21 @@
+package com.example.tungnt.Lesson1_RecyclerView.data;
+
+/**
+ * Created by nguyen on 10/05/2017.
+ */
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
